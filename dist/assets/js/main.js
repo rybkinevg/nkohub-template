1dca2ee658b121bae31d5d5708b11271d74b0688
rybkinevg <boomuk-mc@yandex.ru>
2021-01-18 12:58:49 +0300

    dasdasd

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,7 +3,7 @@ import './_vars';
 import { resizeContent } from './functions/resize';
 import { scrollTo } from './functions/smooth-scroll';
 import { disableScroll, enableScroll } from './functions/stop-scroll';
+import './functions/burger';
 
 //disableScroll(fix) // fix -> class of element with position: fixed
 
-console.log(123);
